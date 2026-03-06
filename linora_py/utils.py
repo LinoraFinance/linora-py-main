56049f0d2f28ae549a3fb35b31334f3e6f8cf3eb
Linora <55220598+LinoraFinance@users.noreply.github.com>
2026-03-06 05:07:14 +0300

    Support api keys and improve client usability (#130)

--- linora_py/utils.py
+++ linora_py/utils.py
@@ -9,6 +9,5 @@ def time_now_milli_secs() -> int:
 def time_now_micro_secs() -> int:
     return int(time.time() * 1_000_000)
 
-
 def raise_value_error(message: str) -> NoReturn:
     raise ValueError(message)
