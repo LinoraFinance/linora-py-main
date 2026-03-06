c1d9adc4add0c68888ab2366adcafa38908b371d
Linora <55220598+LinoraFinance@users.noreply.github.com>
2026-03-06 04:59:30 +0300

    Full coverage of all Linora Websocket channels (#19)

--- linora_py/common/file_logging.py
+++ linora_py/common/file_logging.py
@@ -10,7 +10,8 @@ logging.basicConfig(
     filename=f"logs/{fn_base}_{LOG_TIMESTAMP}.log",
     level=os.getenv("LOGGING_LEVEL", "INFO"),
     format="%(asctime)s.%(msecs)03d | %(levelname)s | %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
+    datefmt="%Y-%m-%d %H:%M:%S"
+    ,
 )
 
 file_logger = logging.getLogger(__name__)
